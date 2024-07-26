a7f81a5f7001e1191bf7bbfca50c024399c7218d
peterhuff <peterhuff@wustl.edu>
2024-07-26 15:47:12 -0400

    Minor form adjustments

--- public/main.js
+++ public/main.js
@@ -8,18 +8,36 @@ const message = document.getElementById("message");
 const contactModal = document.querySelector(".contact-modal");
 const closeButton = document.querySelector(".contact-modal button");
 
-document.addEventListener('invalid', (function(){
-    return function(e) {
-        // prevent default message
-        e.preventDefault();
+// document.addEventListener('invalid', (function(){
+//     return function(e) {
+//         // prevent default message
+//         e.preventDefault();
         
-        // show "required" text
-        const allRequireds = document.getElementsByClassName("required");
-        for (let i = 0; i < 2; i++) {
-            allRequireds[i].style.display = "inline";
-        }
-    };
-})(), true);
+//         // show "required" text
+//         const allRequireds = document.getElementsByClassName("required");
+//         for (let i = 0; i < 2; i++) {
+//             allRequireds[i].style.display = "inline";
+//         }
+//     };
+// })(), true);
+
+fullName.addEventListener("invalid", (e) => {
+    e.preventDefault();
+    const required = document.getElementById("name-required");
+    required.style.display = "inline";
+})
+
+email.addEventListener("invalid", (e) => {
+    e.preventDefault();
+    const required = document.getElementById("email-invalid");
+    required.style.display = "inline";
+})
+
+message.addEventListener("invalid", (e) => {
+    e.preventDefault();
+    const required = document.getElementById("message-required");
+    required.style.display = "inline";
+})
 
 closeButton.addEventListener("click", () => {
     contactModal.close();
@@ -29,14 +47,19 @@ closeButton.addEventListener("click", () => {
 
 contactForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    db.collection("test").add({
+    db.collection("messages").add({
         name: fullName.value,
         email: email.value,
         message: message.value,
-        timeStamp: Date.now()
+        timeStamp: firebase.firestore.FieldValue.serverTimestamp()
     })
     .then(() => {
         contactModal.showModal();
+        contactForm.reset();
+        const allInvalids = document.getElementsByClassName("invalid");
+        for (let i = 0; i < allInvalids.length; i++) {
+            allInvalids[i].style.display = "none";
+        }
         document.body.style.height = '100%';
         document.body.style.overflow = "hidden";    
     })
